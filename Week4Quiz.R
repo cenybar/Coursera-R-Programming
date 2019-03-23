28f3edcec5252914a320f992e48f8f4de990e860
Santiago Fernandez <cenybar@gmail.com>
2019-03-23 12:49:56 +0100

    Add Week4 Quiz first 6 answers

---
+++ Week4Quiz.R
@@ -0,0 +1,17 @@
+1 # What is produced at the end of this snippet of R code?
+
+set.seed(1)
+rpois(5, 2)
+
+#A vector with the numbers 1, 1, 2, 4, 1
+#A vector with the numbers 3.3, 2.5, 0.5, 1.1, 1.7
+# Correct answer # It is impossible to tell because the result is random
+#A vector with the numbers 1, 4, 1, 1, 5
+
+2 # What R function can be used to generate standard Normal random variables?
+    
+#pnorm
+rnorm
+#qnorm
+#dnorm    
+
