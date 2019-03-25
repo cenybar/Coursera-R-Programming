68119bc150a54a8b9c4750f3c491bb3df0ecf01d
Santiago Fernandez <cenybar@gmail.com>
2019-03-25 13:44:10 +0100

    Add best function

--- Week4ProgrammingAssignment.R
+++ Week4ProgrammingAssignment.R
@@ -26,5 +26,45 @@ best <- function(state, outcome) {
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
     ## Check that state and outcome are valid
     states <- unique(data[, 7])
+    outcomes <- c("heart attack", "heart failure", "pneumonia")
+    if ((state %in% states) == FALSE) {
+        stop(print("invalid state"))
+    }
+    else if ((outcome %in% outcomes) == FALSE) {
+        stop(print("invalid outcome"))
+    }
     ## Return hospital name in that state with lowest 30-day death rate
+    # Get the data subset for the desired state
+    state_data <- subset(data, State == state)
+    # Get the desired outcome column from data
+    if (outcome == "heart attack") {
+        outcome_column <- 11
+    }
+    else if (outcome == "heart failure") {
+        outcome_column <- 17
+    }
+    else {
+        outcome_column <- 23
+    }
+    # Work only with complete cases (remove NA's from sample)
+    selected_column <- as.numeric(state_data[,outcome_column])
+    incomplete_cases <- is.na(selected_column)
+    desired_data <- state_data[!incomplete_cases, ]
+    # Find the hospitals with the minimum outcome value
+    columns_considered <- as.numeric(desired_data[,outcome_column])
+    desired_rows <- which(columns_considered == min(columns_considered))
+    desired_hospitals <- desired_data[desired_rows,2]
+    # Tie solution following alphabetical order
+    if (length(desired_hospitals) > 1) {
+        hospitals_sorted <- sort(desired_hospitals)
+        hospitals_sorted[1]
+    }
+    else {
+        desired_hospitals
+    }
 }
+    
+    
+    
+    
+    
